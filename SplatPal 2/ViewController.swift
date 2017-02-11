60ff4131cb5c727c8e509994533ca860ad7e71d3
Kevin Sullivan <kevin@Kevins-MacBook-Pro-2.local>
2017-02-11 18:06:00 -0500

    Initial commit

---
+++ SplatPal 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SplatPal 2
+//
+//  Created by Kevin Sullivan on 2/11/17.
+//  Copyright © 2017 Kevin Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
